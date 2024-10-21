1a7e2e031780c5b2c2f8aeea95a6bc4d85d460ab
Gurram Monika <115149504+Monikag54@users.noreply.github.com>
2024-10-21 21:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+  
+
+
+var header = document.getElementById("navmenu");
+var lists = header.getElementsByClassName("navtabs");
+for (var i = 0; i < lists.length; i++) {
+  lists[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
